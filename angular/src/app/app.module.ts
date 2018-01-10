cade768f412bbaeebe0ff42e29cc60453fbf7624
Julian Poma <poma.nob@gmail.com>
2018-01-10 12:28:17 -0300

    sign in form

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule
